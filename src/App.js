687e2e43e03166c1fb613927ec0266b5cd3a21be
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-13 17:05:05 +0200

    Code push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PrintElement from './PrintElement';
+import './App.css'
+
+const App = ({ ...props }) => {
+
+    return (
+        <div>
+            <PrintElement />
+         </div>
+                
+    )
+}
+
+export default App;
