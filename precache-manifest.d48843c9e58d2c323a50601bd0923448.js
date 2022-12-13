afca23ca1b90f35c4c1a573feb560d1abf8b9ad7
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-13 17:06:40 +0200

    Updates

---
+++ precache-manifest.d48843c9e58d2c323a50601bd0923448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdc16f05a8b79d9e3532b9f32f5bd63",
+    "url": "/BrowserNativePrint/index.html"
+  },
+  {
+    "revision": "1745cdf8cc7a0e35fd90",
+    "url": "/BrowserNativePrint/static/css/main.9aa4e6d0.chunk.css"
+  },
+  {
+    "revision": "4e124cb5809be9208a45",
+    "url": "/BrowserNativePrint/static/js/2.f5b83d2c.chunk.js"
+  },
+  {
+    "revision": "1745cdf8cc7a0e35fd90",
+    "url": "/BrowserNativePrint/static/js/main.86d7b415.chunk.js"
+  },
+  {
+    "revision": "18714f5c0e36978d5ee1",
+    "url": "/BrowserNativePrint/static/js/runtime-main.faa23516.js"
+  },
+  {
+    "revision": "260d57586012b0ed1ae78accc0bf7083",
+    "url": "/BrowserNativePrint/static/media/logo512.260d5758.png"
+  }
+]);
